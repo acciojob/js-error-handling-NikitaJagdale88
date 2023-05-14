1abadf89c0920ba936e3f101394949d7004591ba
Nikita Jagdale <128221436+NikitaJagdale88@users.noreply.github.com>
2023-05-14 19:37:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,59 @@
 //your code here
 
 class OutOfRangeError extends Error {
-	constructor(params) {
-		super(params);
-		this.name = "OutOfRangeError";
-	}
+  constructor() {
+    super();
+    this.name = this.constructor.name;
+    this.message = "Expression should only consist of integers and +-/* characters and not <arg>";
+  }
 }
 
-class InvalidExprError extends Error{
-	constructor(params) {
-		super(params);
-		this.name = "InvalidExprError";
-	} 
-} 
-
-let btn = document.getElementById("btn");
-
-btn.addEventListener('click', (e)=>{
-	let input = document.getElementById("input1").value;
-	let exprarray = input.split("");
-try {
-	let op = ["+", "/", "*", "-"]
-	op.map(op=>{
-		if(exprarray[0] == op || exprarray[exprarray.length-1] == op)
-		throw new OutOfRangeError("Expression should not start with invalid operator");
-	})
-	
-	
-	if(exprarray.includes("a"))
-		throw new InvalidExprError("Expression should only consist of integers and +-/* characters and not ");
-	document.write(eval(input));
-	
-} catch (Error) {
-	document.write(Error.message);
+class InvalidExprError extends Error {
+  constructor() {
+    super();
+    this.name = this.constructor.name;
+    this.message = "Expression should not have an invalid combination of expression";
+  }
 }
 
+function evalString(expression) {
+  if (expression.match(/[-+*/]{2}/g)) {
+    throw new InvalidExprError();
+  }
 
-	
-});
+  if (expression.match(/^[+*/]/)) {
+    throw new SyntaxError("Expression should not start with an invalid operator");
+  }
+
+  if (expression.match(/[-+*/]$/)) {
+    throw new SyntaxError("Expression should not end with an invalid operator");
+  }
+
+  const numbers = expression.split(/[-+*/]/);
+  for (let i = 0; i < numbers.length; i++) {
+    if (!/^[-+]?\d+$/.test(numbers[i].trim())) {
+      throw new OutOfRangeError();
+    }
+  }
+
+  const operators = expression.split(/\d+/).filter(Boolean);
+  for (let i = 0; i < operators.length; i++) {
+    if (!/[-+*/]/.test(operators[i].trim())) {
+      throw new OutOfRangeError();
+    }
+  }
+
+  // Evaluate the expression
+  const result = eval(expression);
+  return result;
+}
+
+// Testing the evalString function
+/*try {
+  const expression = "10 + 5 - 2 * 3 / 4";
+  const result = evalString(expression);
+  console.log("Result:", result);
+} catch (error) {
+  console.error("Error:", error.message);
+}
+*/
