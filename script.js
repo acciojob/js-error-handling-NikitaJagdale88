d23ee664cc69cb61206b02379f62db800151a09d
Nikita Jagdale <128221436+NikitaJagdale88@users.noreply.github.com>
2023-05-14 19:18:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,40 @@
 //your code here
+
 class OutOfRangeError extends Error {
-  constructor(arg) {
-    super(`Expression should only consist of integers and +-/* characters and not ${arg}`);
-    this.name = "OutOfRangeError";
-  }
+	constructor(params) {
+		super(params);
+		this.name = "OutOfRangeError";
+	}
 }
 
-class InvalidExprError extends Error {
-  constructor() {
-    super("Expression should not have an invalid combination of expression");
-    this.name = "InvalidExprError";
-  }
+class InvalidExprError extends Error{
+	constructor(params) {
+		super(params);
+		this.name = "InvalidExprError";
+	} 
+} 
+
+let btn = document.getElementById("btn");
+
+btn.addEventListener('click', (e)=>{
+	let input = document.getElementById("input1").value;
+	let exprarray = input.split("");
+try {
+	let op = ["+", "/", "*", "-"]
+	op.map(op=>{
+		if(exprarray[0] == op || exprarray[exprarray.length-1] == op)
+		throw new OutOfRangeError("Expression should not start with invalid operator");
+	})
+	
+	
+	if(exprarray.includes("a"))
+		throw new InvalidExprError("Expression should only consist of integers and +-/* characters and not ");
+	document.write(eval(input));
+	
+} catch (Error) {
+	document.write(Error.message);
 }
 
 
-function evalString(str) {
-  try {
-    if (/[\+\-\*\/]{2,}/.test(str)) {
-      throw new InvalidExprError();
-    }
-    if (/^[\+\/\*]/.test(str)) {
-      throw new SyntaxError("Expression should not start with invalid operator");
-    }
-    if (/[\+\/\*\-]$/.test(str)) {
-      throw new SyntaxError("Expression should not end with invalid operator");
-    }
-    let result = eval(str);
-    if (!Number.isInteger(result)) {
-      throw new OutOfRangeError(result);
-    }
-    return result;
-  } catch (error) {
-    if (error instanceof OutOfRangeError || error instanceof InvalidExprError) {
-      console.error(`${error.name}: ${error.message}`);
-    } else {
-      throw error;
-    }
-  }
-}
-
-// example usage:
-console.log(evalString("1+2*3-4/2")); // expected output: 5
-console.log(evalString("1.5+2")); // expected output: OutOfRangeError
-console.log(evalString("1++2")); // expected output: InvalidExprError
-console.log(evalString("+1+2")); // expected output: SyntaxError
-console.log(evalString("1+2-")); // expected output: SyntaxError
+	
+});
